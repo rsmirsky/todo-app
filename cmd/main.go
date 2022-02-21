87825fa58bf6df472360c961bfb7f94deffd8a6c
seephp <Smirsky@ukr.net>
2022-02-21 16:05:19 +0200

    create services and dep inj

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,23 @@
 package main
 
-import "github.com/seephp/todo"
-import "log"
+// https://www.youtube.com/watch?v=Q9hl2oSo0i0&list=PLbTTxxr-hMmyFAvyn7DeOgNRN8BQdjFm8&index=2
+import (
+	"log"
+
+	"github.com/seephp/todo"
+	"github.com/seephp/todo/pkg/handler"
+	"github.com/seephp/todo/pkg/service"
+	"github.com/seephp/todo/pkg/repository"
+)
 
 func main() {
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 srv := new(todo.Server)
-if err := srv.Run("8000"); err!= nil {
+if err := srv.Run("8000", handler.InitRoutes()); err!= nil {
 	log.Fatalf("error occured wile running http server: %s", err.Error())
 }
 
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,5 +1,46 @@
 package handler
 
-type Hundler struct{
-	
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/seephp/todo/pkg/service"
+)
+
+type Handler struct {
+	service *service.Service
+}
+
+func (h *Handler) InitRoutes() *gin.Engine{
+	router := gin.New()
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sign-up", h.signUp)
+		auth.POST("/sign-in", h.signIn)
+	}
+	api := router.Group("/api")
+	{
+		lists := api.Group("/lists")
+		{
+			lists.POST("/", h.createList)
+			lists.GET("/", h.getAllLists)
+			lists.GET("/:id", h.getListById)
+			lists.PUT("/:id", h.updateList)
+			lists.DELETE("/:id", h.deleteList)
+
+			items := lists.Group(":id/items")
+			{
+				items.POST("/", h.createItem)
+				items.GET("/", h.getAllItems)
+				items.GET("/:item_id", h.getItemById)
+				items.PUT("/:item_id", h.updateItem)
+				items.DELETE("/:item_id", h.deleteItem)
+			}
+
+		}
+	}
+	return router
 }
